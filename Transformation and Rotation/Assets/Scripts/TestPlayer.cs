cc62e6a59dd6c01954da44bd764d00d57d691c4d
Md. Rakibul Asib Redoy <mdrakibulasibredoy@gmail.com>
2024-12-01 22:02:30 +0600

    Coding Setup Plater

--- Transformation and Rotation/Assets/Scripts/TestPlayer.cs
+++ Transformation and Rotation/Assets/Scripts/TestPlayer.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class TestPlayer : MonoBehaviour
 {
+    float _moveSpeed = 10f;
+    float _rotateSpeed = 200f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,18 @@ public class TestPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(new Vector2(0f, _moveSpeed) * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Rotate(new Vector3(0f, 0f, _rotateSpeed) * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Rotate(new Vector3(0f, 0f, -_rotateSpeed) * Time.deltaTime, Space.Self);
+        }
+
     }
 }
